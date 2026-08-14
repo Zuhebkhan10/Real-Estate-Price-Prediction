44d2929681ec13c01731916228608e3f63236c6a
Zuheb khan <zuhebk433@gmail.com>
2026-08-14 19:43:57 +0530

    update the task

--- server/util.py
+++ server/util.py
@@ -50,3 +50,4 @@ if __name__ == '__main__':
     print(get_estimated_price('1st Phase JP Nagar', 1000, 2, 2))
     print(get_estimated_price('Kalhalli', 1000, 2, 2)) # other location
     print(get_estimated_price('Ejipura', 1000, 2, 2))  # other location
+    print(get_estimated_price('Ejipura', 1000, 2, 2))  # other location
